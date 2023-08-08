13b56e0ef044753c80b23b5e04f6064f439f0015
Shreyanshi123 <85137961+Shreyanshi123@users.noreply.github.com>
2023-08-09 01:29:11 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,17 +1,20 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
 const hre = require("hardhat");
 
 async function main() {
-  // Deploy the contract with constructor arguments
-  const PokemonContract = await hre.ethers.getContractFactory("PokemonContract");
-  const baseTokenURI = "https://gateway.pinata.cloud/ipfs/QmYTkrE8PaLvTaG5NNM7Q5DPFXEA5Pzb4CkuES1D6DK3YK/?_gl=1*1yv469*_ga*Mjg2NTM3OTMuMTY5MTE4MjQxNQ..*_ga_5RMPXG14TE*MTY5MTI2ODYwMy41LjEuMTY5MTI2OTA1MC42MC4wLjA."; // Replace this with your desired base URI
 
-  // Deploy the contract
-  const contract = await PokemonContract.deploy("Pokemon NFT", "PNFT", baseTokenURI);
-  await contract.deployed();
+  const NFT = await hre.ethers.deployContract("PokemonContract");
 
-  console.log("PokemonContract deployed to:", contract.address);
+  console.log("Token address:", await NFT.getAddress());
 }
 
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
